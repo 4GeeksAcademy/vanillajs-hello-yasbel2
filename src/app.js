ee59b968d57d96cac75bccb7dc5703fab7196321
Yabel90 <yasminabelhaj@hotmail.com>
2024-09-13 14:05:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El perro", "Mi Abuela", "El Cartero", "Mi pájaro"];
+  let action = ["comió", "orinó", "aplastó", "rompió"];
+  let what = ["mis deberes", "mi telefono", "El coche"];
+  let when = [
+    "Antes de clase",
+    "Cuando estaba durmiendo",
+    "Cuando estaba haciendo ejercicio",
+    "Mientras comía",
+    "Cuando estaba rezando"
+  ];
+  let whoindice = Math.floor(Math.random() * who.length);
+  let actionindice = Math.floor(Math.random() * action.length);
+  let whatindice = Math.floor(Math.random() * what.length);
+  let whenindice = Math.floor(Math.random() * when.length);
+  document.querySelector(".excuse").innerHTML =
+    who[whoindice] +
+    " " +
+    action[actionindice] +
+    " " +
+    what[whatindice] +
+    " " +
+    when[whenindice];
 };
